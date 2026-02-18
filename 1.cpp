ff33f671feb280d669a59318551b1d224b4325f6
whosthis74 <inglepratik126@gmail.com>
2026-02-18 23:29:35 +0530

    adding 2nd task

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 20;
+    float b = 35000.0;
+    char grade = 'A';
+    bool isPass = true;
+    cout<<a<<b<<grade<<isPass;
+    
+    return 0;
+}
