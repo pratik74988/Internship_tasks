cd44c220a84f7c54e4130e8f9920473290be1ce9
whosthis74 <inglepratik126@gmail.com>
2026-02-19 12:50:47 +0530

    made changes to models.py

--- task2/model.py
+++ task2/model.py
@@ -3,3 +3,33 @@ import torch.nn as nn
 import torchvision.models as models
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+vgg = models.vgg19(pretrained = True).features.to(device).eval()
+
+for param in vgg.parameters():
+    param.requires_grad = False
+
+def gram_matrix (tensor):
+    b, c ,h ,w = tensor.size()
+    features = tensor.view(c, h * w) 
+    gram = torch.mm (features, features.t())
+    return gram
+
+def get_features(image, model):
+    layers = {
+        '0': 'conv1_1',
+        '5': 'conv2_1',
+        '10': 'conv3_1',
+        '19': 'conv4_1',
+        '21': 'conv4_2',  # content layer
+        '28': 'conv5_1'
+    }
+
+    features = {}
+    x = image
+    for name, layer in models._modules.items():
+        x = layer(x)
+        if name in layers:
+            features[layers[name]] = x
+    return features
+
